070a0745bcbc0829c7992bb06d667db60af7e7b4
Saurav-Raj18 <sauravrajbittu95@gmail.com>
2024-11-15 13:07:33 +0530

    fix cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,9 @@ const cookieParser=require('cookie-parser')
 
 app.use(express.json());
 app.use(cors({
-    origin: ['https://car-management-system-orpin.vercel.app/','http://localhost:3000/'],
-    credentials: true,
-
-}));
+    origin: ['http://localhost:3000', 'https://car-management-system-orpin.vercel.app'], // allowed origins
+    credentials: true, // enable cookies and credentials in requests
+  }));
 // app.use(cors());
 app.use(express.json({limit:"160kb"})) //accept json in backend
 app.use(express.urlencoded({extended:true,limit:"160kb"}))//for url 
