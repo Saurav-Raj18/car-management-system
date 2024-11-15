5cbe48b97cdc0f72deb893ad8506342a7c373a6e
Saurav Raj <99171159+Saurav-Raj18@users.noreply.github.com>
2024-11-15 10:37:48 +0530

    Update index.js

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 const { DB_NAME } = require('../constants.js')
 //console.log(process.env.MONGODB_URI)
-
+//console.log("hi")
 const dbConnect = async () => {
     try {
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
